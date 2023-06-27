857fb05dcb094d6185e0e9ebed7f8026c558525c
Zero-Louise <goketsugi002@outlook.com>
2023-06-27 20:03:13 +0800

    the fourth edition of proj1,but not finished part of guitarHero

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -3,7 +3,7 @@ package deque;
 import java.util.Comparator;
 
 
-public class MaxArrayDeque<T> extends ArrayDeque {
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
 
     private Comparator<T> comparator;
 
