fa65b66182fc11addbaeb9874585c6f51de5b2a9
Zero-Louise <goketsugi002@outlook.com>
2023-06-28 00:50:01 +0800

    the first edition of proj1ec

---
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -0,0 +1,44 @@
+package tester;
+
+import static org.junit.Assert.*;
+
+import edu.princeton.cs.algs4.StdRandom;
+import org.junit.Test;
+import student.StudentArrayDeque;
+
+public class TestArrayDequeEC {
+
+    @Test
+    public void randomCallTest() {
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+        StringBuilder msg = new StringBuilder();
+        for (int i = 0; i < 1000; i += 1) {
+            int operator = StdRandom.uniform(0, 4);
+            int randVal = StdRandom.uniform(100);
+            if (operator == 0) {
+                sad.addFirst(randVal);
+                ads.addFirst(randVal);
+                msg.append("addFirst(" + randVal + ")\n");
+                assertEquals(msg.toString(), sad.size(), ads.size());
+            } else if (operator == 1) {
+                sad.addLast(randVal);
+                ads.addLast(randVal);
+                msg.append("addLast(" + randVal + ")\n");
+                assertEquals(msg.toString(), sad.size(), ads.size());
+            } else if (!sad.isEmpty()) {
+                if (operator == 2) {
+                    Integer val1 = sad.removeFirst();
+                    Integer val2 = ads.removeFirst();
+                    msg.append("removeFirst(): " + val2 + "\n");
+                    assertEquals(msg.toString(), val1, val2);
+                } else if (operator == 3) {
+                    Integer val1 = sad.removeLast();
+                    Integer val2 = ads.removeLast();
+                    msg.append("removeLast(): " + val2 + "\n");
+                    assertEquals(msg.toString(), val1, val2);
+                }
+            }
+        }
+    }
+}
